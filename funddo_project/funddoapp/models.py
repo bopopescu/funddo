938cee5cd6a45ae90e9de9223342fc635786b868
dannyxboi <danielorrego02@gmail.com>
2017-01-24 21:30:10 -0500

    progress

--- funddo_project/funddoapp/models.py
+++ funddo_project/funddoapp/models.py
@@ -1,21 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 
-class UserProfile(models.Model):
-	user = models.OneToOneField(User)
-	picture = models.ImageField(upload_to='profile_images', blank=True)
+class UserProfile(AbstractUser):
+	is_donor = models.BooleanField(default=False, verbose_name='Donor')
+	is_recipient = models.BooleanField(default=False, verbose_name='Recipient')
 	bio = models.TextField(blank=True)
+	services = models.TextField(blank=True)
+	image = models.ImageField(upload_to='profile_images', blank=True)
+	shop = models.CharField(max_length=25, blank=True)
+	#location = models
 
-	def __unicode__(self):
-		return self.user.username
 
-class Request(models.Model):
-	poster = models.ForeignKey(User)
-	title = models.CharField(max_length=128)
-	request = models.TextField()
-	posted_on = models.DateTimeField(auto_now_add=True)
+# class Request(models.Model):
+# 	poster = models.ForeignKey(User)
+# 	title = models.CharField(max_length=128)
+# 	request = models.TextField()
+# 	posted_on = models.DateTimeField(auto_now_add=True)
 	
 
-	def __unicode__(self):
-		return self.title
+# 	def __unicode__(self):
+# 		return self.title
